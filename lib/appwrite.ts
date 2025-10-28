a514947f3527dadd0cc9fbe2d14213291fe33ab1
Felipe Castro <carloscastro1860@gmail.com>
2025-10-28 09:32:02 -0500

    chore: config conection to appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+import { Account, Client } from "react-native-appwrite";
+
+// Initialize the Appwrite client
+const client = new Client()
+  .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+export const account = new Account(client);
