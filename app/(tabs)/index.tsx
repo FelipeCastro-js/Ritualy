b2e9f6bfb264dcf10df9e0e343a854507636eeec
Felipe Castro <carloscastro1860@gmail.com>
2025-10-28 19:51:48 -0500

    feat: add new functionality to component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import {
   AppwriteRealtimeResponse,
   client,
+  COMPLETIONS_COLLECTION_ID,
   database,
   DB_ID,
   HABITS_COLLECTION_ID,
@@ -8,15 +9,18 @@ import {
 import { useAuth } from "@/lib/auth-context";
 import { Habit } from "@/types/database.type";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
-import { Query } from "react-native-appwrite";
+import { ID, Query } from "react-native-appwrite";
+import { Swipeable } from "react-native-gesture-handler";
 import { Button, Surface, Text } from "react-native-paper";
 
 export default function Index() {
   const { signOut, user } = useAuth();
 
   const [habits, setHabits] = useState<Habit[]>();
+
+  const swipeableRefs = useRef<{ [key: string]: Swipeable | null }>({});
 
   useEffect(() => {
     if (!user) return;
@@ -56,6 +60,61 @@ export default function Index() {
       console.error(error);
     }
   };
+
+  const renderRightActions = () => (
+    <View style={styles.swipeActionRight}>
+      <MaterialCommunityIcons
+        name="check-circle-outline"
+        size={32}
+        color={"#fff"}
+      />
+    </View>
+  );
+  const renderLeftActions = () => (
+    <View style={styles.swipeActionLeft}>
+      <MaterialCommunityIcons
+        name="trash-can-outline"
+        size={32}
+        color={"#fff"}
+      />
+    </View>
+  );
+
+  const handleDeleteHabit = async (id: string) => {
+    try {
+      await database.deleteDocument(DB_ID, HABITS_COLLECTION_ID, id);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleCompletionsHabit = async (id: string) => {
+    if (!user) return;
+    try {
+      const currentDate = new Date().toISOString();
+      await database.createDocument(
+        DB_ID,
+        COMPLETIONS_COLLECTION_ID,
+        ID.unique(),
+        {
+          habit_id: id,
+          user_id: user?.$id,
+          completed_at: currentDate,
+        }
+      );
+
+      const habit = habits?.find((h) => h.$id === id);
+      if (!habit) return;
+
+      await database.updateDocument(DB_ID, HABITS_COLLECTION_ID, id, {
+        streak_count: habit.streak_count + 1,
+        last_completed: currentDate,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -75,30 +134,52 @@ export default function Index() {
           </View>
         ) : (
           habits?.map((habit, key) => (
-            <Surface key={key} style={styles.card} elevation={0}>
-              <View style={styles.cardContent}>
-                <Text style={styles.cardTitle}> {habit.title}</Text>
-                <Text style={styles.cardDescription}>{habit.description}</Text>
-                <View style={styles.cardFooter}>
-                  <View style={styles.streakBadge}>
-                    <MaterialCommunityIcons
-                      name="fire"
-                      size={18}
-                      color={"#ff9800"}
-                    />
-                    <Text style={styles.streakText}>
-                      {habit.streak_count} day streak
-                    </Text>
-                  </View>
-                  <View style={styles.frequencyBadge}>
-                    <Text style={styles.frequencyText}>
-                      {habit.frequency.charAt(0).toUpperCase() +
-                        habit.frequency.slice(1)}
-                    </Text>
+            <Swipeable
+              ref={(ref) => {
+                swipeableRefs.current[habit.$id] = ref;
+              }}
+              key={key}
+              overshootLeft={false}
+              overshootRight={false}
+              renderLeftActions={renderLeftActions}
+              renderRightActions={renderRightActions}
+              onSwipeableOpen={(direction) => {
+                if (direction === "left") {
+                  handleDeleteHabit(habit.$id);
+                } else if (direction === "right") {
+                  handleCompletionsHabit(habit.$id);
+                }
+
+                swipeableRefs.current[habit.$id]?.close();
+              }}
+            >
+              <Surface key={key} style={styles.card} elevation={0}>
+                <View style={styles.cardContent}>
+                  <Text style={styles.cardTitle}> {habit.title}</Text>
+                  <Text style={styles.cardDescription}>
+                    {habit.description}
+                  </Text>
+                  <View style={styles.cardFooter}>
+                    <View style={styles.streakBadge}>
+                      <MaterialCommunityIcons
+                        name="fire"
+                        size={18}
+                        color={"#ff9800"}
+                      />
+                      <Text style={styles.streakText}>
+                        {habit.streak_count} day streak
+                      </Text>
+                    </View>
+                    <View style={styles.frequencyBadge}>
+                      <Text style={styles.frequencyText}>
+                        {habit.frequency.charAt(0).toUpperCase() +
+                          habit.frequency.slice(1)}
+                      </Text>
+                    </View>
                   </View>
                 </View>
-              </View>
-            </Surface>
+              </Surface>
+            </Swipeable>
           ))
         )}
       </ScrollView>
@@ -112,14 +193,12 @@ const styles = StyleSheet.create({
     padding: 16,
     backgroundColor: "#f5f5f5",
   },
-
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     marginBottom: 24,
   },
-
   title: {
     fontWeight: "bold",
   },
@@ -127,7 +206,7 @@ const styles = StyleSheet.create({
   card: {
     marginBottom: 18,
     borderRadius: 18,
-    backgroundColor: "#f2f2fa",
+    backgroundColor: "#f7f2fa",
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.08,
@@ -135,29 +214,28 @@ const styles = StyleSheet.create({
     elevation: 4,
   },
 
+  cardCompleted: {
+    opacity: 0.6,
+  },
   cardContent: {
     padding: 20,
   },
-
   cardTitle: {
     fontSize: 20,
     fontWeight: "bold",
     marginBottom: 4,
     color: "#22223b",
   },
-
   cardDescription: {
     fontSize: 15,
     marginBottom: 16,
     color: "#6c6c80",
   },
-
   cardFooter: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
-
   streakBadge: {
     flexDirection: "row",
     alignItems: "center",
@@ -166,34 +244,49 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     paddingVertical: 4,
   },
-
   streakText: {
     marginLeft: 6,
     color: "#ff9800",
     fontWeight: "bold",
     fontSize: 14,
   },
-
   frequencyBadge: {
     backgroundColor: "#ede7f6",
     borderRadius: 12,
     paddingHorizontal: 12,
     paddingVertical: 4,
   },
-
   frequencyText: {
     color: "#7c4dff",
     fontWeight: "bold",
     fontSize: 14,
   },
-
   emptyState: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-
   emptyStateText: {
     color: "#666666",
+  },
+  swipeActionLeft: {
+    justifyContent: "center",
+    alignItems: "flex-start",
+    flex: 1,
+    backgroundColor: "#e53935",
+    borderRadius: 18,
+    marginBottom: 18,
+    marginTop: 2,
+    paddingLeft: 16,
+  },
+  swipeActionRight: {
+    justifyContent: "center",
+    alignItems: "flex-end",
+    flex: 1,
+    backgroundColor: "#4caf50",
+    borderRadius: 18,
+    marginBottom: 18,
+    marginTop: 2,
+    paddingRight: 16,
   },
 });
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -8,11 +8,13 @@ export const client = new Client()
 export const account = new Account(client);
 export const database = new Databases(client);
 
-export const DB_ID = process.env.EXPO_PUBLIC_DB_ID;
+export const DB_ID = process.env.EXPO_PUBLIC_DB_ID as string;
 
-export const HABITS_COLLECTION_ID =
-  process.env.EXPO_PUBLIC_HABITS_COLLECTION_ID;
+export const HABITS_COLLECTION_ID = process.env
+  .EXPO_PUBLIC_HABITS_COLLECTION_ID as string;
 
+export const COMPLETIONS_COLLECTION_ID = process.env
+  .EXPO_PUBLIC_COMPLETIONS_COLLECTION as string;
 export type AppwriteRealtimeResponse = {
   events: string[];
   payload: any;
